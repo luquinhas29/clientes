9f63ab2e5b6b6a3f83120defdbc54d79f2890e66
lucas williams <lucaswilliamscesar@gmail.com>
2026-03-28 17:08:36 -0300

    Criado entidade cliente

---
+++ clientes/src/main/java/io/github/luquinhas29/clientes/model/Cliente.java
@@ -0,0 +1,25 @@
+package io.github.luquinhas29.clientes.model;
+
+import lombok.Data;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+
+}
